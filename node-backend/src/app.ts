9fba65357835bffbd7aee575c5b44730de28aada
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2022-05-02 19:26:49 +0200

    Setup Completed

---
+++ node-backend/src/app.ts
@@ -0,0 +1,13 @@
+import express, {Request,Response,Application} from 'express';
+
+const app:Application = express();
+const port = 8180;
+const hostname = '0.0.0.0';
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+app.get('/', (request, response) => {
+    response.send("SUCCESS");
+})

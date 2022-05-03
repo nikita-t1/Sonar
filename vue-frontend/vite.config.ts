bf70329f45de444ee75e5d63ac1bcfbf02194d41
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2022-05-03 21:53:10 +0200

    configured proxy

--- vue-frontend/vite.config.ts
+++ vue-frontend/vite.config.ts
@@ -6,9 +6,21 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 8080,
+    proxy: {
+        '/api': {
+            target: 'http://127.0.0.1:8180',
+            ws: true,
+            secure: false,      
+            changeOrigin: true,
+        }
+    }
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+
 });
